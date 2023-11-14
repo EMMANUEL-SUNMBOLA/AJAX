86f912a45f7689f41625adc31ea9149b1799781a
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-11-15 00:50:49 +0100

    bugs wan kill me

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(isset($_POST["caveman"])){
+            echo $_POST["caveman"];
+        }else{
+            echo "not set";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +16,7 @@
     <title>Training</title>
 </head>
 <body>
-    <button data="caveBtn" id="Btn">Don't Click Me</button>
+    <button data-caveman="caveBtn" id="Btn">Don't Click Me</button>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <!-- CDN link to Jquery -->
     <script src="main.js"></script>
--- main.js
+++ main.js
@@ -2,10 +2,28 @@ $(document).ready(function(){
 //$(document).ready means the script should only activate the function when the page has finished loading.....
     $('#Btn').click(function(){
     // we can use $("then any selector we want I guess..... like css and javascript getElementBy .....")
-        let name = $(this).data('data')
+        let name = $(this).data('caveman')
     // $(this) is signalling to the function that the element that activated the function should be fetched ....
         sendToPhp(name);
     // sendToPhp will send the data to my php script for handling
     //I'm learning AJAX because of my project, I need to send data from the home page to a subsection of the page after they click the button.... so I will be focusing more on how I will get AJAX to work with PHP how i Want it to
     })
 })
+
+function sendToPhp(name){
+    $.ajax({
+        type: "POST",
+    //POST method
+        url: "index.php",
+    // Data will be sent to
+        data: {caveman: name},
+    // Data to be sent
+        success: function(response) {
+            // Handle the response without reloading the page
+            console.log("Response from PHP:", response);
+        },
+        error: function(error) {
+            console.error("Error:", error);
+        }
+    });
+}
