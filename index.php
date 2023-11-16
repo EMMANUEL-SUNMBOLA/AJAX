9181690fce1529f40a9f1d14a4fe0abe69979045
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-11-16 10:58:54 +0100

    day 3, still lost

--- index.php
+++ index.php
@@ -17,7 +17,9 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script>
         $(document).ready(function () {
-            $('.addToCartBtn').click(function () {
+            $('.addToCartBtn').click(function (event) {
+                event.preventDefault(); // Prevent the default button click behavior
+
                 let name = $(this).data('caveman');
                 sendToPhp(name);
             });
@@ -31,9 +33,6 @@
                 success: function (response) {
                     // Reload the content of the div with class 'disp' after the AJAX request is complete
                     $('.disp').html(response);
-
-                    // Now, fetch the button data again and update the div
-                    $('.disp').load('index.php .disp');
                 },
                 error: function (error) {
                     console.error("Error:", error);
--- main.js
+++ main.js
@@ -1,52 +1,52 @@
-$(document).ready(function(){
-//$(document).ready means the script should only activate the function when the page has finished loading.....
-    $('#Btn').click(function(){
-    // we can use $("then any selector we want I guess..... like css and javascript getElementBy .....")
-        let name = $(this).data('caveman');
-    // $(this) is signalling to the function that the element that activated the function should be fetched ....
-        sendToPhp(name);
-    // sendToPhp will send the data to my php script for handling
-    //I'm learning AJAX because of my project, I need to send data from the home page to a subsection of the page after they click the button.... so I will be focusing more on how I will get AJAX to work with PHP how i Want it to
-    })
-})
+// $(document).ready(function(){
+// //$(document).ready means the script should only activate the function when the page has finished loading.....
+//     $('#Btn').click(function(){
+//     // we can use $("then any selector we want I guess..... like css and javascript getElementBy .....")
+//         let name = $(this).data('caveman');
+//     // $(this) is signalling to the function that the element that activated the function should be fetched ....
+//         sendToPhp(name);
+//     // sendToPhp will send the data to my php script for handling
+//     //I'm learning AJAX because of my project, I need to send data from the home page to a subsection of the page after they click the button.... so I will be focusing more on how I will get AJAX to work with PHP how i Want it to
+//     })
+// })
 
-function sendToPhp(name){
-    $.ajax({
-        type: "POST",
-    //POST method
-        url: "ajax.php",
-    // Data will be sent to
-        data: {caveman: name},
-    // Data to be sent
-        success: function(response) {
-            // Handle the response without reloading the page
-            $('.disp').html(response);
-            console.log("Response from PHP:", response);
+// function sendToPhp(name){
+//     $.ajax({
+//         type: "POST",
+//     //POST method
+//         url: "ajax.php",
+//     // Data will be sent to
+//         data: {caveman: name},
+//     // Data to be sent
+//         success: function(response) {
+//             // Handle the response without reloading the page
+//             $('.disp').html(response);
+//             console.log("Response from PHP:", response);
         
-            $('.disp').load('index.php .disp', { caveman: updatedName });
-        // partial reload to make the page load new data without having to reload the entire page
-        },
-        error: function(error) {
-            console.error("Error:", error);
-        }
-    });
-}
+//             $('.disp').load('index.php .disp', { caveman: updatedName });
+//         // partial reload to make the page load new data without having to reload the entire page
+//         },
+//         error: function(error) {
+//             console.error("Error:", error);
+//         }
+//     });
+// }
 
-function sendToPhp(name) {
-    $.ajax({
-        type: "POST",
-        url: "index.php",
-        data: { caveman: name },
-        success: function (response) {
-            // Reload the content of the div with class 'disp' after the AJAX request is complete
-            $('.disp').html(response);
+// function sendToPhp(name) {
+//     $.ajax({
+//         type: "POST",
+//         url: "index.php",
+//         data: { caveman: name },
+//         success: function (response) {
+//             // Reload the content of the div with class 'disp' after the AJAX request is complete
+//             $('.disp').html(response);
 
-            // Now, fetch the button data again and update the div
-            let updatedName = $('#Btn').data('caveman');
-            $('.disp').load('index.php .disp', { caveman: updatedName });
-        },
-        error: function (error) {
-            console.error("Error:", error);
-        },
-    });
-}
+//             // Now, fetch the button data again and update the div
+//             let updatedName = $('#Btn').data('caveman');
+//             $('.disp').load('index.php .disp', { caveman: updatedName });
+//         },
+//         error: function (error) {
+//             console.error("Error:", error);
+//         },
+//     });
+// }
